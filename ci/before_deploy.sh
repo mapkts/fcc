6f2d985ca2fc94b1eb21f402ba18bcfe9f0aa22c
mapkts <mapkts@gmail.com>
2020-07-17 15:17:05 +0800

    deps: move testing libraries to dev-dependencies

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,7 +17,7 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cross rustc --bin fcc --target $TARGET --release
+    cross rustc --bin fcc --target $TARGET --release -- -C lto
 
     cp target/$TARGET/release/fcc $stage/
 
